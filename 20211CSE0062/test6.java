b47ac7d348e8ff15a6674c920c50370cf8fa6131
dinesh0062 <125641900+dinesh0062@users.noreply.github.com>
2023-02-27 09:42:31 +0530

    Create test6.java

---
+++ 20211CSE0062/test6.java
@@ -0,0 +1,16 @@
+//program on unchecked exception (not serious)
+//ex (ArithmeticException, nullpointer,Arrayindexoutofbounds)
+package classobjectDEEMO;
+
+public class test6 {
+
+	public static void main(String[] args) {
+		System.out.println("program begins");
+		int x=5;
+		int y=0;
+		int z=x/y;   //run time mistake
+		System.out.println("end of the program");
+
+	}
+
+}
